3655e4ba74d7b90f7c1ea5345e9e4d773def88a2
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-10-15 11:21:48 +0530

    Annotated the entity

--- app/src/main/java/com/iamjosephvarghese/androidjetpackcomponents/Word.java
+++ app/src/main/java/com/iamjosephvarghese/androidjetpackcomponents/Word.java
@@ -1,9 +1,16 @@
 package com.iamjosephvarghese.androidjetpackcomponents;
 
 import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
+@Entity(tableName = "word_table")
 public class Word {
 
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
     private String mWord;
 
     public Word(@NonNull String mWord) {
