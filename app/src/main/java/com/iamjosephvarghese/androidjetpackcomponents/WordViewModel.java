91a8a695fd805434b5c15732a5ac9c561af11390
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-10-15 12:10:50 +0530

    created word view model

---
+++ app/src/main/java/com/iamjosephvarghese/androidjetpackcomponents/WordViewModel.java
@@ -0,0 +1,28 @@
+package com.iamjosephvarghese.androidjetpackcomponents;
+
+import android.app.Application;
+
+import java.util.List;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+public class WordViewModel extends AndroidViewModel {
+    private WordRepository mRepository;
+    private LiveData<List<Word>> mAllWords;
+
+    public WordViewModel (Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+
+    public LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    public void insert(Word word) {
+        mRepository.insert(word);
+    }
+}
