9a4f6e85a4becc548f233b974084eac4a4943fbf
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-10-15 11:57:08 +0530

    completed wordrepository

---
+++ app/src/main/java/com/iamjosephvarghese/androidjetpackcomponents/WordRepository.java
@@ -0,0 +1,48 @@
+package com.iamjosephvarghese.androidjetpackcomponents;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+
+public class WordRepository {
+
+    private WordDao mWordDao;
+    private LiveData<List<Word>> mAllWords;
+
+
+    WordRepository(Application application) {
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
+        mWordDao = db.wordDao();
+        mAllWords = mWordDao.getAlphabetizedWords();
+    }
+
+
+    public LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+
+    public void insert (Word word) {
+        new insertAsyncTask(mWordDao).execute(word);
+    }
+
+
+    private static class insertAsyncTask extends AsyncTask<Word, Void, Void> {
+
+        private WordDao mAsyncTaskDao;
+
+        insertAsyncTask(WordDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Word... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+
+}
