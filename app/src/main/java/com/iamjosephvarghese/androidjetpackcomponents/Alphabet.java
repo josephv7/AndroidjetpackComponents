a5f580f3bb6074b1fc364cf6e9a1ca21f9eaf77a
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-10-16 10:33:33 +0530

    created the entinty using Alphabet calss

---
+++ app/src/main/java/com/iamjosephvarghese/androidjetpackcomponents/Alphabet.java
@@ -0,0 +1,32 @@
+package com.iamjosephvarghese.androidjetpackcomponents;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.room.ColumnInfo;
+import androidx.room.Dao;
+import androidx.room.Entity;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.PrimaryKey;
+import androidx.room.Query;
+
+@Entity(tableName = "alphabet_table")
+public class Alphabet {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "alphabet")
+    private String mAlphabet;
+
+    public Alphabet(@NonNull String mAlphabet) {
+        this.mAlphabet = mAlphabet;
+    }
+
+
+    @NonNull
+    public String getmAlphabet() {
+        return mAlphabet;
+    }
+}
