afdb6a26dc72d8496317a0e5c7a6468f06bb7f2e
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-10-16 11:28:42 +0530

    added necessary functions to repository

--- app/src/main/java/com/iamjosephvarghese/androidjetpackcomponents/WordRepository.java
+++ app/src/main/java/com/iamjosephvarghese/androidjetpackcomponents/WordRepository.java
@@ -10,13 +10,17 @@ import androidx.lifecycle.LiveData;
 public class WordRepository {
 
     private WordDao mWordDao;
+    private AlphabetDao mAlphabetDao;
     private LiveData<List<Word>> mAllWords;
+    private LiveData<List<Alphabet>> mAllAlphabets;
 
 
     WordRepository(Application application) {
         WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
         mWordDao = db.wordDao();
+        mAlphabetDao = db.alphabetDao();
         mAllWords = mWordDao.getAlphabetizedWords();
+        mAllAlphabets = mAlphabetDao.orderAscending();
     }
 
 
@@ -24,9 +28,19 @@ public class WordRepository {
         return mAllWords;
     }
 
+    public LiveData<List<Alphabet>> getAllAlphabets() {
+        return mAllAlphabets;
+    }
+
+
 
     public void insert (Word word) {
         new insertAsyncTask(mWordDao).execute(word);
+    }
+
+
+    public void insert (Alphabet alphabet){
+        new insertAsyncTaskAlphabet(mAlphabetDao).execute(alphabet);
     }
 
 
@@ -40,6 +54,22 @@ public class WordRepository {
 
         @Override
         protected Void doInBackground(final Word... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+
+
+    private static class insertAsyncTaskAlphabet extends AsyncTask<Alphabet, Void, Void> {
+
+        private AlphabetDao mAsyncTaskDao;
+
+        insertAsyncTaskAlphabet(AlphabetDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(Alphabet... params) {
             mAsyncTaskDao.insert(params[0]);
             return null;
         }
